2520f37e09f512dbb8ab8d7b8976dc826a8000be
brian <brian@contactology.com>
2012-08-07 09:02:13 -0400

    Loudbot script now caches louds to a local file instead of calling the API every time. This makes launching new shells much faster.

--- bin/loudbot.py
+++ bin/loudbot.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python
 
+import os
 import signal
 import urllib
 
 def handler(signum, frame):
-    raise Exception("Alarm")
+    raise RuntimeError("Alarm")
 signal.signal(signal.SIGALRM, handler)
-signal.alarm(3)
+
+def update_louds():
+    if os.fork():
+        signal.alarm(3)
+        print urllib.urlopen("http://isuckatdomains.net:3168/loud.pl").read()
+    else:
+        signal.alarm(45)
+        louds = []
+        for i in range(10):
+            louds.append(urllib.urlopen("http://isuckatdomains.net:3168/loud.pl").read())
+
+        with open(os.getenv("HOME") + "/.louds-new", "w") as f:
+            for loud in louds:
+                f.write(loud.strip() + "\n")
+
+        os.rename(os.getenv("HOME") + "/.louds-new", os.getenv("HOME") + "/.louds")
+    
 
 try:
-    print urllib.urlopen("http://isuckatdomains.net:3168/loud.pl").read()
-except:
-    pass
+    with open(os.getenv("HOME") + "/.louds") as f:
+        louds = list(f)
+
+    if len(louds) < 5:
+        update_louds()
+    else:
+        print louds[0]
+        with open(os.getenv("HOME") + "/.louds-new", "w") as f:
+            for loud in louds[1:]:
+                f.write(loud.strip() + "\n")
+        os.rename(os.getenv("HOME") + "/.louds-new", os.getenv("HOME") + "/.louds")
+        
+
+except IOError:
+    try:
+        update_louds()
+    except RuntimeError:
+        pass    
+except RuntimeError:
+    pass    
