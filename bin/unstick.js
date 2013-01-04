312e5200a3d249b44acfed43af719bbe91b21483
spiffytech <spiffytech@gmail.com>
2013-01-04 13:31:50 -0500

    Unstick script now prints each campaign as you unstick it, marks stuck notices read after fetching them

--- bin/unstick.js
+++ bin/unstick.js
@@ -18,7 +18,7 @@ var imap = new ImapConnection({
 var MailParser = require("mailparser").MailParser;
 var mp = new MailParser();
 
-commands = [];
+messages = [];
 
 step(
     function() {
@@ -34,39 +34,57 @@ step(
         imap.search(["UNSEEN", ["SINCE", "December 21, 2012"], ["!HEADER", "SUBJECT", "Unstuck Manually"]], this);
     },
     function(err, results) {
+        if(results.length == 0) {
+            console.log("No campaigns to unstick");
+            imap.logout();
+            return;
+        }
+
         var fetch = imap.fetch(results, {
             request: {
                 headers: ["from", "to", "subject", "date"],
                 body: true
-            }
+            },
+            markSeen: true
         });
-        fetch.on("message", function(msg) {
+
+        fetch.on("message", function(message) {
             var body = "";
 
-            msg.on("data", function(chunk) {
+            message.on("data", function(chunk) {
                 body += chunk.toString("utf8");
             });
 
-            msg.on("end", function() {
+            message.on("end", function() {
                 var matches = body.match(/command=(.*--client [0-9]+ --campaign [0-9]+)/);
                 if(matches) {
-                    console.log(msg.headers.subject[0]);
-                    console.log(msg.headers.date[0]);
-                    console.log(matches[1]);
                     cmd = matches[1];
-                    commands.push(cmd);
+                    message.command = cmd;
+                    messages.push(message);
                 }
             });
         });
 
         fetch.on("end", function() {
-            console.log("All done.");
-            imap.logout();
-            if(commands.length > 0) {
-                exec(commands, function(err, stdout, stderr) {});
-            } else {
-                console.log("No campaigns to unstick");
-            }
+            console.log("Unsticking " + messages.length + " stuck messages");
+            unstick_campaigns(messages);
         });
     }
 );
+
+var unstick_campaigns = function(messages) {
+    if(messages.length > 0) {
+        message = messages[0];
+        messages.shift();
+
+        console.log("");
+        console.log(message.headers.subject[0] + " " + message.headers.date[0]);
+        console.log(message.command);
+
+        exec(message.command, function(err, stdout, stderr) {});
+        unstick_campaigns(messages);
+    } else {
+        console.log("No more campaigns to unstick");
+        imap.logout();
+    }
+}
