8b3bb8202106293470067c18d59ad5d67e811ee1
spiffytech <spiffytech@gmail.com>
2015-02-04 11:08:51 -0500

    More or less enable web-mode for Emacs, thus correctly highlighting .erb templates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -90,6 +90,18 @@
 (unless (package-installed-p 'nyan-mode)
   (package-install 'nyan-mode))
 
+(unless (package-installed-p 'web-mode)
+  (package-install 'web-mode))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
 (global-visual-line-mode t)
 
 (setq frame-title-format
