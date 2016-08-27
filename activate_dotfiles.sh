eeb47140b1ce1b67dfcb8fe0e440a2a759ba11b2
spiffytech <spiffytech@gmail.com>
2016-08-27 16:34:08 -0400

    feature(tmux): enable session autosave, plus nifty copy/paste plugin

--- activate_dotfiles.sh
+++ activate_dotfiles.sh
@@ -6,3 +6,10 @@ if [ `whoami` = "root" ]; then
 fi
 
 for f in `ls -A ~/dotfiles-public | grep -v .git | grep -v activate_dotfiles.sh`; do rm -rf ~/$f; ln -s ~/dotfiles-public/$f ~/; done
+
+# tmux plugin manager
+has_git=`hash git 2>/dev/null`
+has_git=$?
+if [[ $has_git && ! -d ~/.tmux/plugins/tpm ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
