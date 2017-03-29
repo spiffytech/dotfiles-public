cc34fca8c2469c9976c79015c576080ff133038e
spiffytech <spiffytech@gmail.com>
2017-03-29 10:08:54 -0400

    fix(activation): copy in .gitconfig

--- activate_dotfiles.sh
+++ activate_dotfiles.sh
@@ -5,7 +5,7 @@ if [ `whoami` = "root" ]; then
     exit 1
 fi
 
-for f in `ls -A ~/dotfiles-public | grep -v .git | grep -v activate_dotfiles.sh`; do rm -rf ~/$f; ln -s ~/dotfiles-public/$f ~/; done
+for f in `ls -A ~/dotfiles-public | grep -v '^.git$' | grep -v activate_dotfiles.sh`; do rm -rf ~/$f; ln -s ~/dotfiles-public/$f ~/; done
 
 # tmux plugin manager
 has_git=`hash git 2>/dev/null`
